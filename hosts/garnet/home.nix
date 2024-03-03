b1bd637e259b0834434820f6296d08b540d31f9d
Matt Muldowney <matt.muldowney@gmail.com>
2024-03-03 01:01:56 -0800

    garnet: desktop entries

--- hosts/garnet/home.nix
+++ hosts/garnet/home.nix
@@ -1,4 +1,15 @@
 { inputs, user, ... }:
+let
+  webDesktopEntry = { name, website ? "${name}.com" }: {
+    inherit name;
+    value = {
+      inherit name;
+      exec = "firefox ${website}";
+      terminal = false;
+      icon = name;
+    };
+  };
+in
 {
   imports = [
       inputs.self.outputs.homeManagerModules.default
@@ -20,6 +31,37 @@
   dconf = {
     enable = true;
     settings."org/gnome/desktop/interface".color-scheme = "prefer-dark";
+  };
+
+  xdg.desktopEntries = builtins.listToAttrs (map webDesktopEntry [
+    { name = "netflix"; }
+    { name = "youtube"; }
+    { name = "hulu"; }
+    { name = "streameast"; website = "streameast.xyz"; }
+  ] );
+
+  # xdg.desktopEntries = {
+  #   netflix = {
+  #     name = "Netflix";
+  #     exec = "firefox netflix.com";
+  #     terminal = false;
+  #     icon = "netflix2";
+  #   };
+
+  #   youtube = {
+  #     name = "YouTube";
+  #     exec = "firefox youtube.com";
+  #     terminal = false;
+  #     icon = "youtube";
+  #   };
+  # };
+
+  xdg.dataFile = {
+    icons = {
+      enable = true;
+      target = "icons";
+      source = ./icons;
+    };
   };
 
   # Let Home Manager install and manage itself.
