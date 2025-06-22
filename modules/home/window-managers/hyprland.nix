24590e7bc989c86ec123a01d8c100495704b3168
Matt Muldowney <matt.muldowney@gmail.com>
2025-06-22 00:00:45 -0700

    hyprland: no need for ctrl-caps switch since done in keyboard firmware

--- modules/home/window-managers/hyprland.nix
+++ modules/home/window-managers/hyprland.nix
@@ -61,7 +61,6 @@ in
           kb_layout = "us";
           kb_variant = "";
           kb_model = "";
-          kb_options = "ctrl:swapcaps";
           kb_rules = "";
           follow_mouse = 1;
           touchpad.natural_scroll = false;
